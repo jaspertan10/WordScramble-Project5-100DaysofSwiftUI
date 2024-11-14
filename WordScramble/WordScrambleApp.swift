1f0d5a7f84e40da0ced449ac7381b77be9068d0d
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-14 10:40:19 -0800

    initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jasper Tan on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
