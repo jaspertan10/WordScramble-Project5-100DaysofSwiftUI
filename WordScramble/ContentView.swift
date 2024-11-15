a1b6915110f710682d624f490915afd21494471b
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-14 20:22:15 -0800

    wordScramble base functionality complete

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -13,6 +13,11 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    //Alerts
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    @State private var alertTitle: String = ""
+    
     var body: some View {
 
         NavigationStack {
@@ -34,10 +39,86 @@ struct ContentView: View {
                 
             }
             .navigationTitle(rootWord)
+            .onAppear {
+                startGame()
+            }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+
         }
         .preferredColorScheme(.dark)
         
         
+    }
+    
+    func errorMessage(errorTitle: String, errorMessage: String) {
+        alertMessage = errorMessage
+        alertTitle = errorTitle
+        showAlert = true
+    }
+    
+    func isOriginal(_ word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(_ word: String) -> Bool {
+        
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let index = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(_ word: String) -> Bool {
+        
+        // Create instance of UITextChecker
+        let checker = UITextChecker()
+        
+        // Create an Obj-C string range using length of our characters in utf16 format
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        // Check where the any mispellings are in a word
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        // If no mispellings found, return True, i.e. isReal word
+        return mispelledRange.location == NSNotFound
+    }
+    
+    func startGame() {
+        
+        /*
+        1. Find start.txt in our bundle
+        2. Load it into a string
+        3. Split that string into array of strings, with each element being one word
+        4. Pick one random word from there to be assigned to rootWord, or use a sensible default if the array is empty.
+         */
+        
+        var wordArray: [String]
+        
+        if let wordFile = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            //Able to find file
+            if let wordString = try? String(contentsOf: wordFile, encoding: .ascii) {
+                //store words in array
+                wordArray = wordString.components(separatedBy: "\n")
+                
+                if let randomWord = wordArray.randomElement() {
+                    rootWord = randomWord
+                    return
+                }
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
     
     func addNewWord() {
@@ -46,6 +127,22 @@ struct ContentView: View {
         guard answer.count > 0 else {
             return
         }
+        
+        guard isOriginal(answer) else {
+            errorMessage(errorTitle: "Word not original", errorMessage: "Enter a new word")
+            return
+        }
+        
+        guard isPossible(answer) else {
+            errorMessage(errorTitle: "Word not possible", errorMessage: "You can't spell that word from \(rootWord)")
+            return
+        }
+        
+        guard isReal(answer) else {
+            errorMessage(errorTitle: "Word not real", errorMessage: "Enter a real word")
+            return
+        }
+    
         
         withAnimation {
             usedWords.insert(answer, at: 0)
